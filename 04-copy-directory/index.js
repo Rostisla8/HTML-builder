44ed37fe7a5a0c3ee76c9283e8aa1cf942d6fde2
Rostisla8 <86406897+Rostisla8@users.noreply.github.com>
2022-05-23 21:36:34 +0300

    docs: change solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,88 +1,41 @@
 
+const fs = require('fs');
 const path = require('path');
-const fsp = require('fs/promises');
-const { join } = require('path');
+const pathF = path.join(__dirname, 'files');
+const pathFCopy = path.join(__dirname, 'files-copy');
 
-const getFiles = async (folder) => {
-    try {
-        const files = [];
-        const entries = await fsp.readdir(path.join(__dirname, folder), {
-            withFileTypes: true,
+const copyDirPath = (pathFolder, pathFCopy) => {
+  fs.mkdir(pathFCopy, { recursive: true }, (error) => {
+    if (error) console.log(error);
+  });
+
+  fs.readdir(pathFCopy, (error, data) => {
+    if (error) console.log(error);
+    data.forEach((item) => {
+      fs.access(path.join(pathFolder, item), (error) => {
+        if (error) {
+          fs.rm(path.join(pathFCopy, item), { recursive: true, force: true}, (error)   => {
+            if (error) console.log(error);
+          });
+        }
+      });
+    });
+  });
+
+  fs.readdir(pathFolder, {withFileTypes: true}, (error, data,) => {
+    if (error) console.log(error);
+    data.forEach(file => {
+      if(file.isFile()) {
+        fs.copyFile(path.join(pathFolder, file.name), path.join(pathFCopy, file.name),   (error) =>{
+          if (error) console.log(error);
         });
-        for (const file of entries) {
-            if (file.isFile()) {
-                if (file.name !== '.gitkeep')
-                    files.push({ name: file.name, path: folder });
-            } else if (file.isDirectory()) {
-                const newFiles = await getFiles(path.join(folder, file.name));
-                if (newFiles.length) files.push(...newFiles);
-            }
-        }
-        return files;
-    } catch (error) {
-        console.log(error);
-    }
+      }
+      if(file.isDirectory()) {
+        copyDirPath(path.join(pathFolder, file.name), path.join(pathFCopy, file.name));
+      }
+    });
+    console.log('Folder is created!');
+  });
 };
 
-const copyFiles = async (src, dest) => {
-    const files = await getFiles(src);
-    const folders = [
-        dest,
-        ...files
-            .map((file) => file.path)
-            .filter((value) => value !== src)
-            .filter((value, index, arr) => arr.indexOf(value) === index)
-            .map((str) => str.replace(src, dest)),
-    ];
-
-    const cleanFolder = async (dir) => {
-        try {
-            await fsp.access(path.join(__dirname, dir));
-            const entries = await fsp.readdir(path.join(__dirname, dir), {
-                withFileTypes: true,
-            });
-            if (!entries.length) return;
-            for (const entrie of entries) {
-                await fsp.rm(path.join(__dirname, dir, entrie.name), {
-                    recursive: true,
-                });
-            }
-        } catch (err) {/* try...catch access */}
-    };
-
-    const createFolder = async (path) => {
-        try {
-            await fsp.access(path);
-        } catch (error) {
-            await fsp.mkdir(path);
-        }
-    };
-
-    await cleanFolder(dest);
-
-    for (const folder of folders) {
-        const subfolders = folder.split(path.sep);
-        for (let i = 0; i <= subfolders.length; i++) {
-            if (subfolders.slice(0, [i]).length) {
-                const folderPath = path.join(
-                    __dirname,
-                    ...subfolders.slice(0, [i])
-                );
-                await createFolder(folderPath);
-            }
-        }
-    }
-
-    for (const file of files) {
-        fsp.copyFile(
-            path.join(__dirname, file.path, file.name),
-            path.join(
-                __dirname,
-                file.path.replace(src, dest),
-                file.name
-            )
-        );
-    }
-};
-
-copyFiles('files', 'files-copy');
+copyDirPath(pathF, pathFCopy);
